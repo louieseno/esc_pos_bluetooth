136de4f29e4d1b543e8aeb7c5267f8577e6640c7
louieseno <senolouiejohn@gmail.com>
2020-11-20 10:51:35 +0800

    test: append timeout 2 for scan

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -114,7 +114,7 @@ class PrinterBluetoothManager {
     }
 
     // We have to rescan before connecting, otherwise we can connect only once
-    await _bluetoothManager.startScan(timeout: Duration(seconds: 1));
+    await _bluetoothManager.startScan(timeout: Duration(seconds: _timeOut + 2));
     await _bluetoothManager.stopScan();
 
     // Connect
